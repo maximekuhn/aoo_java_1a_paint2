4adf3e030ecf582a0099eb6093b1cdc8eaa18295
Albi <albi.grainca@uha.fr>
2022-04-28 23:13:19 +0200

    changement des dimensions toolbar

--- graphics/shapes/ui/toolbar/ToolContainer.java
+++ graphics/shapes/ui/toolbar/ToolContainer.java
@@ -46,7 +46,7 @@ public abstract class ToolContainer {
 	}
 	
 	public ImageIcon imageSize(String filename) {
-		return new ImageIcon(new ImageIcon(filename).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+		return new ImageIcon(new ImageIcon(filename).getImage().getScaledInstance(23, 23, Image.SCALE_DEFAULT));
 	}
 	
 	public Image cursorSize(String filename) {
