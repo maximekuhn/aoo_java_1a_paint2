4c2fba607c4feb89a5695e0b72e8d33776ed4688
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2022-05-05 21:03:05 +0200

    reset ShapesController to original

--- graphics/shapes/ui/ShapesController.java
+++ graphics/shapes/ui/ShapesController.java
@@ -14,20 +14,12 @@ public class ShapesController extends Controller {
 	
 	private Point lastClick;
 	private boolean shiftDown;
-	private boolean handlerSelected;
-	private int handler;
-
-	private static final int HANDLER_SIZE = 12;
-	private static final int MIN_SHAPE_SIZE = 6;
 	
 	public ShapesController(Object model) {
 		super(model);
 		this.lastClick = new Point();
 		this.shiftDown = false;
-		this.handlerSelected = false;
-		this.handler = 0;
 	}
-	
 	
 	@Override
 	public void mousePressed(MouseEvent e)
@@ -51,7 +43,6 @@ public class ShapesController extends Controller {
 	
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		this.handlerSelected = false;
 	}
 	
 	@Override
@@ -61,7 +52,6 @@ public class ShapesController extends Controller {
 		int dy = evt.getY() - this.lastClick.y;
 		
 		this.translateSelected(dx, dy);
-		this.resizeSelected(dx, dy);
 		
 		this.lastClick.setLocation(evt.getPoint());
 	}
@@ -111,70 +101,10 @@ public class ShapesController extends Controller {
 			Shape s = it.next();
 			SelectionAttributes sa = (SelectionAttributes) s.getAttributes(SelectionAttributes.ID);
 			if(sa.isSelected()) {
-				if (!handlerSelected) {
-					this.handler = this.isHandlerSelected(s);
-					this.handlerSelected = true;
-				}
-				if (this.handler==0) {
-					s.translate(dx, dy);
-					this.getView().repaint();
-				}
+				s.translate(dx, dy);
+				this.getView().repaint();
 			}
 		}
 	}
 	
-	private void resizeSelected(int dx, int dy) {
-		SCollection model = (SCollection) this.getModel();
-		Iterator<Shape> it = model.iterator();
-		
-		while(it.hasNext()) {
-			Shape s = it.next();
-			SelectionAttributes sa = (SelectionAttributes) s.getAttributes(SelectionAttributes.ID);
-			if(sa.isSelected()) {
-				if (!handlerSelected) {
-					this.handler = this.isHandlerSelected(s);
-					this.handlerSelected = true;
-				}
-				if (handler==1) {
-					if (s.getBounds().width-dx < MIN_SHAPE_SIZE && dx > 0) {
-						dx = 0;
-					}
-					if (s.getBounds().height-dy < MIN_SHAPE_SIZE && dy > 0) {
-						dy = 0;
-					}
-					s.translate(dx, dy);
-					s.resize(-dx, -dy);
-					this.getView().repaint();
-				}
-				else if (handler==2) {
-					if (s.getBounds().width-dx < MIN_SHAPE_SIZE && dx < 0) {
-						dx = 0;
-					}
-					if (s.getBounds().height-dy < MIN_SHAPE_SIZE && dy < 0) {
-						dy = 0;
-					}
-					s.resize(dx, dy);
-					this.getView().repaint();
-				}
-			}
-		}
-	}
-
-	private int isHandlerSelected(Shape s) {
-		int xleft = s.getBounds().x;
-		int xright = s.getBounds().x + s.getBounds().width;
-		int ytop = s.getBounds().y;
-		int ybottom = s.getBounds().y + s.getBounds().height;
-		if (this.lastClick.x >= xleft-HANDLER_SIZE && this.lastClick.x <= xleft && this.lastClick.y >= ytop-HANDLER_SIZE && this.lastClick.y <= ytop) {
-			// if top left handler is selected
-			return 1;
-		}
-		else if (this.lastClick.x >= xright && this.lastClick.x <= xright+HANDLER_SIZE && this.lastClick.y >= ybottom && this.lastClick.y <= ybottom+HANDLER_SIZE) {
-			// if bottom right handler is selected
-			return 2;
-		}
-		else {
-			return 0;
-		}
-	}
 }
