87c4878565cb32f323c08bc5a5db12fea74f0b31
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2022-05-14 19:44:26 +0200

    mb fix for SKotlin

--- graphics/shapes/SKotlin.java
+++ graphics/shapes/SKotlin.java
@@ -2,6 +2,7 @@ package graphics.shapes;
 
 import java.awt.Point;
 import java.awt.Polygon;
+import java.util.StringJoiner;
 
 import graphics.shapes.attributes.ColorAttributes;
 import graphics.shapes.attributes.LayerAttributes;
@@ -55,6 +56,44 @@ public class SKotlin extends SPolygon {
 		if(la == null) la = new LayerAttributes();
 		sk.addAttributes(new LayerAttributes(la.getLayer()));
 		return sk;
+	}
+	
+	@Override
+	public String toString() {
+		/*
+		 * take copy, because copy always have all attributes
+		 * (no need to check)
+		 */
+		SKotlin sk = (SKotlin) this.copy();
+		StringJoiner tmp = new StringJoiner(" ", "[ " , " ]");
+
+		// basic
+		tmp.add(sk.getClass().getName());
+		tmp.add(String.valueOf(sk.getLoc().x));
+		tmp.add(String.valueOf(sk.getLoc().y));
+		tmp.add(String.valueOf(sk.getBounds().width));
+		tmp.add(String.valueOf(sk.getBounds().height));
+
+		// color attributes
+		ColorAttributes ca = (ColorAttributes) sk.getAttributes(ColorAttributes.ID);
+		tmp.add(String.valueOf(ca.filled));
+		tmp.add(String.valueOf(ca.stroked));
+		tmp.add(String.valueOf(ca.filledColor.getRGB()));
+		tmp.add(String.valueOf(ca.strokedColor.getRGB()));
+
+		// selection attributes
+		SelectionAttributes sa = (SelectionAttributes) sk.getAttributes(SelectionAttributes.ID);
+		tmp.add(String.valueOf(sa.isSelected()));
+
+		// rotation attributes
+		RotationAttributes ra = (RotationAttributes) sk.getAttributes(RotationAttributes.ID);
+		tmp.add(String.valueOf(ra.getAngle()));
+
+		// layer attributes
+		LayerAttributes la = (LayerAttributes) sk.getAttributes(LayerAttributes.ID);
+		tmp.add(String.valueOf(la.getLayer()));
+
+		return tmp.toString();
 	}
 
 
